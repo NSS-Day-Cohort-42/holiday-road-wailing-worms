55080e4af1a854b807ca18455fc92464316bf230
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-07 13:28:57 -0500

    removed full date and added just a return of the text version of a date

--- scripts/weather/WeatherHTMLConvertor.js
+++ scripts/weather/WeatherHTMLConvertor.js
@@ -1,9 +1,10 @@
+const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
 export const weatherHTMLRep = (weatherObj) => {
   const kelvinConvert = Math.round(((`${weatherObj.temp.day}`-273.15)*1.8)+32)
   return`
   <div class="weatherContainerOutput">
-    <div>${new Date((weatherObj.dt*1000)).toLocaleDateString()}</div>
+    <div>${days[new Date((weatherObj.dt*1000)).getDay()]}</div>
     <div>Avg. Temp:</div>
     <div>${kelvinConvert}&degF</div>
     <div>${weatherObj.weather[0].main}</div>
