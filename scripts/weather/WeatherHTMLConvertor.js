d3710fc0fda0957f56dd35710533aa093cc5a4a8
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-07 08:10:47 -0500

    got temp added

--- scripts/weather/WeatherHTMLConvertor.js
+++ scripts/weather/WeatherHTMLConvertor.js
@@ -1,5 +1,15 @@
+
 export const weatherHTMLRep = (weatherObj) => {
+  const kelvinConvert = Math.round(((`${weatherObj[2].temp.day}`-273.15)*1.8)+32)
   return`
-    ${weatherObj[0].dt}
+  <div>Date:</div>
+  <div>${new Date((weatherObj[0].dt*1000)).toLocaleDateString()}</div>
+  <div>Avg. Temp:</div>
+  <div>${kelvinConvert}&deg Fahrenheit</div>
+
+  <div>Date:</div>
+  <div>${new Date((weatherObj[5].dt*1000)).toLocaleDateString()}</div>
+  <div>Avg. Temp:</div>
+  <div>${kelvinConvert}&deg Fahrenheit</div>
   `
 }
