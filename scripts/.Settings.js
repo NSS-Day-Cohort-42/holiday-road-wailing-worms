10e8f18119dbb5d77f2f624939ed9c4c1a4d6bc3
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-06 21:38:39 -0500

    Fixed API Keys

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,5 @@
 export default {
 	graphhopperKey: "",
-	npsKey: "ptVl4gT1bHNoBs9DKF8soKDBFx9K6xhydJ5OVj9D",
-	weatherKey: "c4beeead5a016ed28a1e4ab704fc98c3"
+	npsKey: "",
+	weatherKey: ""
 }
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,9 +1,11 @@
+import key from "../Settings.js"
+
 let weatherArray = []
 
 export const useWeather = () => weatherArray.slice()
 
 export const getWeather = () => {
-  return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=c4beeead5a016ed28a1e4ab704fc98c3")
+  return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=${key.weatherKey}`)
   .then(res => res.json())
   .then(
     parsedWeather => {
