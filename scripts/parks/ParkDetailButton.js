698aaf083d9390988370ff4c182a7743c27b817b
travisbrigman <travis1000@gmail.com>
2020-08-08 21:32:38 -0500

    image with park full name

--- scripts/parks/ParkDetailButton.js
+++ scripts/parks/ParkDetailButton.js
@@ -1,9 +1,12 @@
+
+import { useParks } from "./ParkProvider.js"
 const eventHub = document.querySelector(".eventHub")
 const contentTarget = document.querySelector(".parkCard")
 
 // On the event hub, listen for a "change" event.
 eventHub.addEventListener("parkSelected", () => {
     ShowParkDetailsButton()
+    getParkCard()
 })
 
 eventHub.addEventListener("click", clickEvent => {
@@ -17,9 +20,32 @@ eventHub.addEventListener("click", clickEvent => {
 })
 
 export const ShowParkDetailsButton = () => {
-    contentTarget.innerHTML = "<button id= 'showParkDetails'>Show Park Details</button>"
+    contentTarget.innerHTML += `<button id= 'showParkDetails'>Show Park Details</button>`
 }
+
 
 const closeModalDialog = () => { 
     document.getElementById("parkDetailDialog").close();
 }
+
+
+export const getParkCard = () => {
+    const nationalParkCode = document.querySelector("#parkSelect").value
+    const parksArray = useParks()
+    
+    const matchedPark = parksArray.find(
+        selectedPark => {
+            return nationalParkCode === selectedPark.parkCode
+        })        
+        
+        console.log(matchedPark)
+        showParkCard(matchedPark)    
+    }
+    
+    export const showParkCard = (selectedParkObject) => {
+        let numberOfImagesInAPI = selectedParkObject.images.length
+        let randomImageSelector = Math.floor(Math.random() * (numberOfImagesInAPI + 1))
+        contentTarget.innerHTML = 
+        `<div>${ selectedParkObject.fullName }</div>
+        <img src="${selectedParkObject.images[randomImageSelector].url}" alt="${selectedParkObject.images[randomImageSelector].altText}" width= 300px height= 300px SameSite=None; Secure> `
+    }
--- scripts/parks/ParkDetailHTMLConvertor.js
+++ scripts/parks/ParkDetailHTMLConvertor.js
@@ -1,16 +1,20 @@
-export const ParkDetailTMLConverter = parkObject => {
-    return `
-        <section class="modalContainer--park">
-        <h5>Address</h5>
-        <div class= "park--addressLocation">${parkObject.addresses[0].line2}</div>
-        <div class= "park--streetAddress">${parkObject.addresses[0].line1}</div>
-        <div class= "park--cityAddress">${parkObject.addresses[0].city}</div>
-        <div class= "park--state">${parkObject.addresses[0].stateCode}</div>
-        <div class= "park--latitude">${parkObject.latitude}</div>
-        <div class= "park--longitude">${parkObject.longitude}</div>
-        <div class= "park--phone">${parkObject.contacts.phoneNumbers[0].phoneNumber}</div>      
-        </section>
-    `
-
-        
-}
+export const ParkDetailTMLConverter = (parkObject) => {
+    if (parkObject.entranceFees.length > 0) {
+      parkObject.entranceFees.forEach((fee) => {
+        fee.cost;
+        fee.title; 
+      });
+    }
+  return `
+    <section class="modalContainer--park">
+    <h5>Address</h5>
+    <div class= "park--addressLocation">${parkObject.addresses[0].line2}</div>
+    <div class= "park--streetAddress">${parkObject.addresses[0].line1}</div>
+    <div class= "park--cityAddress">${parkObject.addresses[0].city}</div>
+    <div class= "park--state">${parkObject.addresses[0].stateCode}</div>
+    <div class= "park--latitude">${parkObject.latitude}</div>
+    <div class= "park--longitude">${parkObject.longitude}</div>
+    <div class= "park--phone">${parkObject.contacts.phoneNumbers[0].phoneNumber}</div>      
+    </section>
+    `;
+};
