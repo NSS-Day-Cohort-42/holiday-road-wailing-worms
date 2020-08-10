a69456e58c2cfb98966ab0e800424394e5ec8c4a
travisbrigman <travis1000@gmail.com>
2020-08-10 10:12:14 -0500

    fixed random image occasional crash

--- scripts/parks/ParkDetailButton.js
+++ scripts/parks/ParkDetailButton.js
@@ -1,5 +1,7 @@
-
+//import the array or park data from the provider module
 import { useParks } from "./ParkProvider.js"
+
+//establish where the eventhub is "looking" and where content will be targeted.
 const eventHub = document.querySelector(".eventHub")
 const contentTarget = document.querySelector(".parkCard")
 
@@ -8,6 +10,7 @@ eventHub.addEventListener("parkSelected", () => {
     getParkCard()
 })
 
+//on the event hub, list for a "click" event.
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "showParkDetails") {
         const detailEvent = new CustomEvent("showParkDetailsClicked")
@@ -18,16 +21,19 @@ eventHub.addEventListener("click", clickEvent => {
     }
 })
 
+//A function that renders a button
 export const ShowParkDetailsButton = () => {
     contentTarget.innerHTML += `<button id= 'showParkDetails'>Show Park Details</button>`
 }
 
-
+//This will close the the modal when you click on anything that isn't in the parkDetailsDialog class.
 const closeModalDialog = () => { 
     document.getElementById("parkDetailDialog").close();
 }
 
-
+/* This function finds the park code that was established in the dropdown and then declares a value of parksArray for the parks data that was imported on line 2.
+Using the find method, the park code is matched to the one that is in each JSON object in the array. The return value of that function is passed into the the showParkCard function.
+The function that renders the park details button is also called. */
 export const getParkCard = () => {
     const nationalParkCode = document.querySelector("#parkSelect").value
     const parksArray = useParks()
@@ -40,13 +46,16 @@ export const getParkCard = () => {
         showParkCard(matchedPark)    
         ShowParkDetailsButton()
     }
-    
+  
+ /* This function takes in a single JSON object from the parks array and then checks if it's image property is greater than zero. Essentially, ensuring data is there to work with.
+ Next we store the number of images that are in the images array in a variable. To create variety, a variable is created that randomly selects a number that is between 0 and the n-1 numbers of images in the images property.
+ lastly, values of the URL and it's alt text are interpolated into an HTML img tag. To handle if there are no images, a locally stored version of the NPS logo will be displayed. */   
     export const showParkCard = (selectedParkObject) => {
         if (selectedParkObject.images.length > 0) {
         let numberOfImagesInAPI = selectedParkObject.images.length
-        let randomImageSelector = Math.floor(Math.random() * (numberOfImagesInAPI + 1))
+        let randomImageSelector = Math.floor(Math.random() * (numberOfImagesInAPI))
         contentTarget.innerHTML = 
-        `<div class="parkName">${ selectedParkObject.fullName }</div>
+        `<div id="parkName">${ selectedParkObject.fullName }</div>
         <img src="${selectedParkObject.images[randomImageSelector].url}" alt="${selectedParkObject.images[randomImageSelector].altText}" width= 300px height= 300px SameSite=None; Secure> `
         } else {
             contentTarget.innerHTML = 
--- scripts/parks/ParkDetailHTMLConvertor.js
+++ scripts/parks/ParkDetailHTMLConvertor.js
@@ -1,10 +1,17 @@
+/* This function interpolates data from a JSON object into HTML strings. 
+Many can be directly interpolated, but some properties in each object needed to be evaluated before there values could be interpolated and rendered.
+For example: entrance fees is an array property on the JSON object that varies from park to park. So first we check and see if the array has any values in it at all.
+If it does, the property is iterated on using the map method and we return interpolated strings that contain specific values we want to display. 
+Each time time the map method finds values in the array, it appends them to a string that started out as empty. This string value is then interpolated into the HTML to be rendered.
+ */
+
 export const ParkDetailTMLConverter = (parkObject) => {
   let parkFees = "";
   if (parkObject.entranceFees.length > 0) {
     const parkFeesInfo = parkObject.entranceFees
       .map((fee) => {
-        return `<div>${fee.title}</div>
-                <div>${Math.round(fee.cost)}</div>`;
+        return `<div><b>Description:</b> ${fee.title}</div>
+                <div><b>Cost (USD)</b> $${Math.round(fee.cost)}</div>`;
       })
       .join("");
     parkFees += parkFeesInfo;
@@ -21,8 +28,6 @@ export const ParkDetailTMLConverter = (parkObject) => {
   }
   return `
     <section class="modalContainer--park">
-    ${parkFees}
-    ${parkOperatingHours}
     <h5>Address</h5>
     <div class= "park--addressLocation">${parkObject.addresses[0].line2}</div>
     <div class= "park--streetAddress">${parkObject.addresses[0].line1}</div>
@@ -31,6 +36,10 @@ export const ParkDetailTMLConverter = (parkObject) => {
     <div class= "park--latitude">${parkObject.latitude}</div>
     <div class= "park--longitude">${parkObject.longitude}</div>
     <div class= "park--phone">${parkObject.contacts.phoneNumbers[0].phoneNumber}</div>      
+    <h5>Fees</h5>
+    ${parkFees}
+    <h5>Operating Hours</h5>
+    ${parkOperatingHours}
     </section>
     `;
 };
