0b0f17fb05a96deaa0cee35121343c0c1ee092aa
Dylan Rowe <dylanthk@gmail.com>
2020-08-10 14:33:46 -0500

    dr-finished Eateries

--- scripts/eateries/EateriesHTMLConvertor.js
+++ scripts/eateries/EateriesHTMLConvertor.js
@@ -1,24 +1,25 @@
 const eventHub = document.querySelector('.eventHub')
 
 eventHub.addEventListener('click', (clickEvent) => {
-    if (clickEvent.target.id.startsWith("eateryDialog--") ) {
-  const[prefix, eateryId] = clickEvent.target.id.split('--')     
+  if (clickEvent.target.id.startsWith("eateryDialog--") ) {
+const[prefix, eateryId] = clickEvent.target.id.split('--')     
 //   this splits the id name from the interpolated section which is the value of whatever restaurants the user selects.
-      // prefix = eateryDialog, eateryId = ${eateryObj.id}. [eateryDialog, eateryObj.id]
-  const eateryDetailEvent = new CustomEvent("eateryDetail", {
-      detail: {
-          eateryId: eateryId
-        //   the value or id of what is selected
-      }
-  })
-  
-        eventHub.dispatchEvent(eateryDetailEvent)
+    // prefix = eateryDialog, eateryId = ${eateryObj.id}. [eateryDialog, eateryObj.id]
+const eateryDetailEvent = new CustomEvent("eateryDetail", {
+    detail: {
+        eateryId: eateryId
+      //   the value or id of what is selected
     }
+})
+
+      eventHub.dispatchEvent(eateryDetailEvent)
+  }
 }) 
+
 
 export const eateriesToHtmlConverter = (eateryObj) => {
      return `
-     <div class="eatery__Container">
+     <div class="">
     <div class="name__Container">${eateryObj.businessName}</div>
     <div> ${eateryObj.description}</div>
      <div class="details__button">
@@ -26,4 +27,4 @@ export const eateriesToHtmlConverter = (eateryObj) => {
      </div>
     </div>`
 }
-// ${eateryObj.id} stores the actual id from whatever object is selected
+// ${eateryObj.id} stores the actual id from whatever object is selected.
